3e7206e369bfed0ddbc316300058375ad94f203d
Ross Waguespack <rosswaguespack@gmail.com>
2017-04-30 18:42:18 -0500

    Attempt at fixing webpack compile in prod

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -11,7 +11,7 @@ const Hello = props => (
 )
 
 Hello.defaultProps = {
-  name: 'David'
+  name: 'Ross'
 }
 
 Hello.propTypes = {
@@ -20,7 +20,7 @@ Hello.propTypes = {
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Hello name='Ross Waguespack' />,
     document.body.appendChild(document.createElement('div')),
   )
 })
